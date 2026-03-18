b52f6eb023f957c8d67c0e2f7cb2fa4cd994c985
ehugues <evanhugues@proton.me>
2026-03-18 17:49:02 +0100

    experimental features

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,22 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import { defineConfig, memoryCache } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	experimental: {
+		rustCompiler: true,
+		queuedRendering: {
+			enabled: true,
+		},
+		cache: {
+			provider: memoryCache(),
+		},
+	},
+	security: {
+		csp: true
+	},
 });
