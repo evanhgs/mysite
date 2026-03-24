4a679bb245412087b1db887ce284d9cb6316b5d6
evan <evanhugues@proton.me>
2026-03-24 11:37:19 +0100

    edit consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,5 @@
 export const SITE_TITLE = 'Evan\'s blogs';
 export const SITE_DESCRIPTION = 'Bienvenue sur mon site';
+export const GITHUB_LINK = 'https://github.com/evanhgs';
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/evanhugues/';
+export const MASTODON = "https://m.webtoo.ls/@astro";
